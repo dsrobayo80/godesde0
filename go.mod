fdb7aaaf56b215f3b3e7551fe415068c87dc1f81
David <davidsantiago.robayovelandia@everis.nttdata.com>
2023-08-24 15:18:45 -0500

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dsrobayo80/godesde0
+
+go 1.21.0
---
+++ variables/enteras.go
@@ -0,0 +1,13 @@
+package variables
+
+import "fmt"
+
+func MostrarEnteros() {
+	var intComun int
+	intde32 := int32(10)
+	intde64 := int64(600)
+
+	fmt.Println("Int comun = ", intComun)
+	fmt.Println("Int de 32 = ", intde32)
+	fmt.Println("Int de 64 = ", intde64)
+}
